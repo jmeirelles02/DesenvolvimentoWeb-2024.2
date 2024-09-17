dc20ee639f0923014b40ca949215d7ff672ff781
João Gabriel Guedes <jogague123@gmail.com>
2024-09-17 08:06:54 -0300

    semana 5

---
+++ aula5-devweb/inicio.js
@@ -0,0 +1,13 @@
+function func1(x, y){
+    return x + y;
+}
+
+const func2 = function (a, b){
+    return a * b;
+}
+
+const func3 = () => {
+    return "Bonito"
+}
+
+const minha_lista = [25, 'casa', [34, 12], 9.65];
